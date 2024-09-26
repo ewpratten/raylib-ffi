cd4ad6c87e07b6192d22a090db5ed7249d8a994a
Evan Pratten <evan@ewpratten.com>
2024-09-26 15:26:56 -0400

    SVG support

--- src/wrapper.h
+++ src/wrapper.h
@@ -1,3 +1,7 @@
 // This file is used to inform bindgen about every header we need to parse
 
+// Configuration variables
+#define SUPPORT_FILEFORMAT_SVG 1
+
+// Load raylib
 #include "../third_party/raylib/src/raylib.h"
