b3d40b73e57517a765bf8b89a6f0aaf748c78370
Evan Pratten <evan@ewpratten.com>
2024-11-22 20:43:48 -0500

    Raylib 5.5

--- src/wrapper.h
+++ src/wrapper.h
@@ -1,7 +1,4 @@
 // This file is used to inform bindgen about every header we need to parse
 
-// Configuration variables
-#define SUPPORT_FILEFORMAT_SVG 1
-
 // Load raylib
 #include "../third_party/raylib/src/raylib.h"
